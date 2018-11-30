81a1579b519a5d4c3da6826d848abeba236a8afb
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-30 23:34:32 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |trait_type, trait_hash|
+    trait_hash.each do |trait, names_array|
+      names_array.each do |name|
+        
+        unless pigeon_list.has_key?(name)
+          pigeon_list[name] = {}
+        end
+        
+        unless pigeon_list[name].has_key?(trait_type)
+          pigeon_list[name][trait_type] = []
+        end
+          
+        unless pigeon_list[name][trait_type].include?(trait)
+          pigeon_list[name][trait_type] << trait.to_s
+        end
+        
+      end
+    end
+  end
+  pigeon_list
 end
