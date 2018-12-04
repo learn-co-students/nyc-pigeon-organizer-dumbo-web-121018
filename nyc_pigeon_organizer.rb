cc438e09f5b34cc51f374bc44051651f2a7ec0bc
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-12-04 21:06:40 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+    data.each_with_object({}) do |(key, info), answer|
+      #binding.pry
+	    info.each do |value, names|
+      names.each do |name|
+        answer[name] ||= {}
+        answer[name][key] ||= []
+        answer[name][key] << value.to_s
+      end
+    end
+  end
 end
