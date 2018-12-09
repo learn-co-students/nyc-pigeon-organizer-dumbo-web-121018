cae5e59009c437ff35939acc155de7545b1b48d8
Joshua Alphonse <joshalphonse@gmail.com>
2018-12-09 17:33:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
+
+  data.each do |attributes, types|
+    types.each do |kind, names|
+        names.each do |name|
+          if result[name].nil?
+            result[name] = {}
+          end
+          if result[name][attributes].nil?
+            result[name][attributes] = []
+          end
+          result[name][attributes] << kind.to_s
+      end
+    end
+  end
+result
 end
